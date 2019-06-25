7a716726416cb8819f8f1dac986c40a3d9fbcafb
vincent929 <vincentcheng929@gmail.com>
2019-06-25 14:18:45 +0800

    convert keras model to ncnn

---
+++ utils/custom_objects.py
@@ -0,0 +1,307 @@
+import keras
+import numpy as np
+import tensorflow as tf
+import keras.backend as K
+from keras import Input
+from keras.engine import Model, Layer, InputSpec
+from keras.layers import DepthwiseConv2D, BatchNormalization, Activation, Conv2D, concatenate, Conv2DTranspose
+from keras.utils import conv_utils
+from keras import backend as K
+from keras.backend import int_shape, permute_dimensions
+
+from utils import loss
+
+def _resize_images(x, height_factor, width_factor, data_format):
+    """Resizes the images contained in a 4D tensor.
+    # Arguments
+        x: Tensor or variable to resize.
+        height_factor: Positive integer.
+        width_factor: Positive integer.
+        data_format: string, `"channels_last"` or `"channels_first"`.
+    # Returns
+        A tensor.
+    # Raises
+        ValueError: if `data_format` is neither `"channels_last"` or `"channels_first"`.
+    """
+    if data_format == 'channels_first':
+        original_shape = int_shape(x)
+        new_shape = tf.shape(x)[2:]
+        new_shape *= tf.constant(np.array([height_factor, width_factor]).astype('int32'))
+        x = permute_dimensions(x, [0, 2, 3, 1])
+        x = tf.image.resize_bilinear(x, new_shape)
+        x = permute_dimensions(x, [0, 3, 1, 2])
+        x.set_shape(
+            (None, None, original_shape[2] * height_factor if original_shape[2] is not None else None,
+             original_shape[3] * width_factor if original_shape[3] is not None else None))
+        return x
+    elif data_format == 'channels_last':
+        original_shape = int_shape(x)
+        new_shape = tf.shape(x)[1:3]
+        new_shape *= tf.constant(np.array([height_factor, width_factor]).astype('int32'))
+        x = tf.image.resize_bilinear(x, new_shape)
+        x.set_shape(
+            (None, original_shape[1] * height_factor if original_shape[1] is not None else None,
+             original_shape[2] * width_factor if original_shape[2] is not None else None, None))
+        return x
+    else:
+        raise ValueError('Invalid data_format:', data_format)
+
+
+class BilinearUpsampling2D(Layer):
+    """Upsampling layer for 2D inputs by using bilinear interpolation.
+    Repeats the rows and columns of the data
+    by size[0] and size[1] respectively.
+    # Arguments
+        size: int, or tuple of 2 integers.
+            The upsampling factors for rows and columns.
+        data_format: A string,
+            one of `channels_last` (default) or `channels_first`.
+            The ordering of the dimensions in the inputs.
+            `channels_last` corresponds to inputs with shape
+            `(batch, height, width, channels)` while `channels_first`
+            corresponds to inputs with shape
+            `(batch, channels, height, width)`.
+            It defaults to the `image_data_format` value found in your
+            Keras config file at `~/.keras/keras.json`.
+            If you never set it, then it will be "channels_last".
+    # Input shape
+        4D tensor with shape:
+        - If `data_format` is `"channels_last"`:
+            `(batch, rows, cols, channels)`
+        - If `data_format` is `"channels_first"`:
+            `(batch, channels, rows, cols)`
+    # Output shape
+        4D tensor with shape:
+        - If `data_format` is `"channels_last"`:
+            `(batch, upsampled_rows, upsampled_cols, channels)`
+        - If `data_format` is `"channels_first"`:
+            `(batch, channels, upsampled_rows, upsampled_cols)`
+    """
+
+    def __init__(self, size=(2, 2), data_format=None, **kwargs):
+        super(BilinearUpsampling2D, self).__init__(**kwargs)
+        self.data_format =K.normalize_data_format(data_format)
+        self.size = conv_utils.normalize_tuple(size, 2, 'size')
+        self.input_spec = InputSpec(ndim=4)
+
+    def compute_output_shape(self, input_shape):
+        if self.data_format == 'channels_first':
+            height = self.size[0] * input_shape[2] if input_shape[2] is not None else None
+            width = self.size[1] * input_shape[3] if input_shape[3] is not None else None
+            return (input_shape[0],
+                    input_shape[1],
+                    height,
+                    width)
+        elif self.data_format == 'channels_last':
+            height = self.size[0] * input_shape[1] if input_shape[1] is not None else None
+            width = self.size[1] * input_shape[2] if input_shape[2] is not None else None
+            return (input_shape[0],
+                    height,
+                    width,
+                    input_shape[3])
+
+    def call(self, inputs, **kwargs):
+        return _resize_images(inputs, self.size[0], self.size[1],
+                              self.data_format)
+
+    def get_config(self):
+        config = {'size': self.size,
+                  'data_format': self.data_format}
+        base_config = super(BilinearUpsampling2D, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+
+class BilinearUpsampling(Layer):
+    """Just a simple bilinear upsampling layer. Works only with TF.
+       Args:
+           upsampling: tuple of 2 numbers > 0. The upsampling ratio for h and w
+           output_size: used instead of upsampling arg if passed!
+    """
+
+    def __init__(self, upsampling=(2, 2), output_size=None, data_format=None, **kwargs):
+
+        super(BilinearUpsampling, self).__init__(**kwargs)
+
+        self.data_format = K.normalize_data_format(data_format)
+        self.input_spec = InputSpec(ndim=4)
+        if output_size:
+            self.output_size = conv_utils.normalize_tuple(
+                output_size, 2, 'output_size')
+            self.upsampling = None
+        else:
+            self.output_size = None
+            self.upsampling = conv_utils.normalize_tuple(
+                upsampling, 2, 'upsampling')
+
+    def compute_output_shape(self, input_shape):
+        if self.upsampling:
+            height = self.upsampling[0] * \
+                input_shape[1] if input_shape[1] is not None else None
+            width = self.upsampling[1] * \
+                input_shape[2] if input_shape[2] is not None else None
+        else:
+            height = self.output_size[0]
+            width = self.output_size[1]
+        return (input_shape[0],
+                height,
+                width,
+                input_shape[3])
+
+    def call(self, inputs):
+        if self.upsampling:
+            return K.tf.image.resize_bilinear(inputs, (inputs.shape[1] * self.upsampling[0],
+                                                       inputs.shape[2] * self.upsampling[1]),
+                                              align_corners=True)
+        else:
+            return K.tf.image.resize_bilinear(inputs, (self.output_size[0],
+                                                       self.output_size[1]),
+                                              align_corners=True)
+
+    def get_config(self):
+        config = {'upsampling': self.upsampling,
+                  'output_size': self.output_size,
+                  'data_format': self.data_format}
+        base_config = super(BilinearUpsampling, self).get_config()
+        return dict(list(base_config.items()) + list(config.items()))
+
+def relu6(x):
+    return K.relu(x, max_value=6)
+
+def _conv_block(inputs, filters, alpha, kernel=(3, 3), strides=(1, 1), block_id=1):
+    """Adds an initial convolution layer (with batch normalization and relu6).
+
+    # Arguments
+        inputs: Input tensor of shape `(rows, cols, 3)`
+            (with `channels_last` data format) or
+            (3, rows, cols) (with `channels_first` data format).
+            It should have exactly 3 inputs channels,
+            and width and height should be no smaller than 32.
+            E.g. `(224, 224, 3)` would be one valid value.
+        filters: Integer, the dimensionality of the output space
+            (i.e. the number output of filters in the convolution).
+        alpha: controls the width of the network.
+            - If `alpha` < 1.0, proportionally decreases the number
+                of filters in each layer.
+            - If `alpha` > 1.0, proportionally increases the number
+                of filters in each layer.
+            - If `alpha` = 1, default number of filters from the paper
+                 are used at each layer.
+        kernel: An integer or tuple/list of 2 integers, specifying the
+            width and height of the 2D convolution window.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+        strides: An integer or tuple/list of 2 integers,
+            specifying the strides of the convolution along the width and height.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+            Specifying any stride value != 1 is incompatible with specifying
+            any `dilation_rate` value != 1.
+
+    # Input shape
+        4D tensor with shape:
+        `(samples, channels, rows, cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(samples, rows, cols, channels)` if data_format='channels_last'.
+
+    # Output shape
+        4D tensor with shape:
+        `(samples, filters, new_rows, new_cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(samples, new_rows, new_cols, filters)` if data_format='channels_last'.
+        `rows` and `cols` values might have changed due to stride.
+
+    # Returns
+        Output tensor of block.
+    """
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+    filters = int(filters * alpha)
+    x = Conv2D(filters, kernel,
+               padding='same',
+               use_bias=False,
+               strides=strides,
+               name='conv_%d' % block_id)(inputs)
+    x = BatchNormalization(axis=channel_axis, name='conv_%d_bn' % block_id)(x)
+    return Activation(relu6, name='conv_%d_relu' % block_id)(x)
+
+
+def _depthwise_conv_block(inputs, pointwise_conv_filters, alpha,
+                          depth_multiplier=1, strides=(1, 1), block_id=1):
+    """Adds a depthwise convolution block.
+
+    A depthwise convolution block consists of a depthwise conv,
+    batch normalization, relu6, pointwise convolution,
+    batch normalization and relu6 activation.
+
+    # Arguments
+        inputs: Input tensor of shape `(rows, cols, channels)`
+            (with `channels_last` data format) or
+            (channels, rows, cols) (with `channels_first` data format).
+        pointwise_conv_filters: Integer, the dimensionality of the output space
+            (i.e. the number output of filters in the pointwise convolution).
+        alpha: controls the width of the network.
+            - If `alpha` < 1.0, proportionally decreases the number
+                of filters in each layer.
+            - If `alpha` > 1.0, proportionally increases the number
+                of filters in each layer.
+            - If `alpha` = 1, default number of filters from the paper
+                 are used at each layer.
+        depth_multiplier: The number of depthwise convolution output channels
+            for each input channel.
+            The total number of depthwise convolution output
+            channels will be equal to `filters_in * depth_multiplier`.
+        strides: An integer or tuple/list of 2 integers,
+            specifying the strides of the convolution along the width and height.
+            Can be a single integer to specify the same value for
+            all spatial dimensions.
+            Specifying any stride value != 1 is incompatible with specifying
+            any `dilation_rate` value != 1.
+        block_id: Integer, a unique identification designating the block number.
+
+    # Input shape
+        4D tensor with shape:
+        `(batch, channels, rows, cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(batch, rows, cols, channels)` if data_format='channels_last'.
+
+    # Output shape
+        4D tensor with shape:
+        `(batch, filters, new_rows, new_cols)` if data_format='channels_first'
+        or 4D tensor with shape:
+        `(batch, new_rows, new_cols, filters)` if data_format='channels_last'.
+        `rows` and `cols` values might have changed due to stride.
+
+    # Returns
+        Output tensor of block.
+    """
+    channel_axis = 1 if K.image_data_format() == 'channels_first' else -1
+    pointwise_conv_filters = int(pointwise_conv_filters * alpha)
+
+    x = DepthwiseConv2D((3, 3),
+                        padding='same',
+                        depth_multiplier=depth_multiplier,
+                        strides=strides,
+                        use_bias=False,
+                        name='conv_dw_%d' % block_id)(inputs)
+    x = BatchNormalization(axis=channel_axis, name='conv_dw_%d_bn' % block_id)(x)
+    x = Activation(relu6, name='conv_dw_%d_relu' % block_id)(x)
+
+    x = Conv2D(pointwise_conv_filters, (1, 1),
+               padding='same',
+               use_bias=False,
+               strides=(1, 1),
+               name='conv_pw_%d' % block_id)(x)
+    x = BatchNormalization(axis=channel_axis, name='conv_pw_%d_bn' % block_id)(x)
+    return Activation(relu6, name='conv_pw_%d_relu' % block_id)(x)
+
+def custom_objects():
+    return {
+        'relu6': relu6, #keras.layers.ReLU(6.),
+        'BilinearUpsampling': BilinearUpsampling,
+        'BilinearUpsampling2D': BilinearUpsampling2D,
+        'dice_coef_loss': loss.dice_coef_loss,
+        'dice_coef': loss.dice_coef,
+        'recall': loss.recall,
+        'precision': loss.precision,
+    }
+
+
